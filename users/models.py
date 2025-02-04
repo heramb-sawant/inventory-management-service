f853c642bfde85ba92dfe7794f5c963d26d4bfc4
Heramb Sawant <herambsawant11@gmail.com>
2025-02-04 15:26:56 -0700

    Added user model and admin portal

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name: models.CharField(max_length=30)
+    last_name: models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.id
+
+    def get_name(self):
+        return f"{self.first_name} {self.last_name}"
