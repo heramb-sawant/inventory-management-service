0a19dcf958166fba4de804e7156444b3a21fa7d3
Heramb Sawant <herambsawant11@gmail.com>
2025-02-04 11:24:53 -0700

    Connect to local postgres instance

--- inventory_management_service/settings.py
+++ inventory_management_service/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "inventory_management_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "inventory_management",
+        "USER": "test",
+        "PASSWORD": "testpassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
