a5ea32c9dacb13166f7e1fafb5faeda85ad3a625
Heramb Sawant <herambsawant11@gmail.com>
2025-02-04 17:28:37 -0700

    Added a few more views

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+
 from . import views
 
 urlpatterns = [
     path("", views.get_users, name="index"),
+    path("<int:user_id>/", views.get_user, name="user"),
+    path("<int:user_id>/details/", views.get_user_details, name="details"),
 ]
--- users/views.py
+++ users/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def get_users(request):
     return HttpResponse("User list to come!")
+
+
+def get_user(request, user_id):
+    return HttpResponse(f"User get to come! {user_id}")
+
+
+def get_user_details(request, user_id):
+    return HttpResponse(f"User details to come! {user_id}")
