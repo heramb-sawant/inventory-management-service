f5cd103ec55c0b36e587d21ce04f584f06ac19a0
Heramb Sawant <herambsawant11@gmail.com>
2025-02-06 13:15:47 -0700

    Fixed users model

--- users/models.py
+++ users/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 class Users(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name: models.CharField(max_length=30)
-    last_name: models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.id
+        return self.first_name
 
     def get_name(self):
         return f"{self.first_name} {self.last_name}"
