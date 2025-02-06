d4af378997d07131b81dfe7c2d5c09a4278f7a34
Heramb Sawant <herambsawant11@gmail.com>
2025-02-06 14:42:01 -0700

    Got user updates working

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from .models import Users
 
@@ -22,9 +23,40 @@ def get_user(request, user_id):
     # return render(request, "users/detail.html", {"user": user})
 
 
-def create_user(request):
-    return HttpResponse("TODO:!")
+def create_user(request, user_id):
+    return HttpResponse(f"User details to come! {user_id}")
+    # question = get_object_or_404(Question, pk=question_id)
+    # try:
+    #     selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    # except (KeyError, Choice.DoesNotExist):
+    #     # Redisplay the question voting form.
+    #     return render(
+    #         request,
+    #         "polls/detail.html",
+    #         {
+    #             "question": question,
+    #             "error_message": "You didn't select a choice.",
+    #         },
+    #     )
+    # else:
+    #     selected_choice.votes = F("votes") + 1
+    #     selected_choice.save()
+    #     # Always return an HttpResponseRedirect after successfully dealing
+    #     # with POST data. This prevents data from being posted twice if a
+    #     # user hits the Back button.
+    #     return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
 
 def update_user(request, user_id):
-    return HttpResponse(f"User details to come! {user_id}")
+    user = get_object_or_404(Users, pk=user_id)
+    updated_first_name = request.GET["first_name"]
+    updated_last_name = request.GET["last_name"]
+    if updated_first_name:
+        user.first_name = updated_first_name
+
+    if updated_last_name:
+        user.last_name = updated_last_name
+
+    user.save()
+
+    return HttpResponseRedirect(reverse("users:details", args=[user.id]))
