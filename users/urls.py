edd62043ebf8a66f75e6db4a805daad0d91f8bc4
Heramb Sawant <herambsawant11@gmail.com>
2025-02-06 12:25:06 -0700

    Added new users details view

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.get_users, name="index"),
-    path("<int:user_id>/", views.get_user, name="user"),
-    path("<int:user_id>/details/", views.get_user_details, name="details"),
+    path("<int:user_id>/", views.get_user, name="details"),
+    path("<int:user_id>/more/", views.get_user_details, name="more"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Users
 
@@ -11,7 +11,14 @@ def get_users(request):
 
 
 def get_user(request, user_id):
-    return HttpResponse(f"User details to come! {user_id}")
+    user = get_object_or_404(Users, pk=user_id)
+    return render(request, "users/detail.html", {"user": user})
+
+    # try:
+    #     user = Users.objects.get(pk=user_id)
+    # except Users.DoesNotExist:
+    #     raise Http404("User does not exist")
+    # return render(request, "users/detail.html", {"user": user})
 
 
 def create_user(request):
