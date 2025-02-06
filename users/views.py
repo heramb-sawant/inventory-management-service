6503f91ead2b538a89165f69a84e5e8250af8622
Heramb Sawant <herambsawant11@gmail.com>
2025-02-06 12:08:43 -0700

    Added html template for user list

--- users/views.py
+++ users/views.py
@@ -1,12 +1,21 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Users
 
 
 def get_users(request):
-    return HttpResponse("User list to come!")
+    most_recent = Users.objects.order_by("-id")[:5]
+    context = {"latest_question_list": most_recent}
+    return render(request, "users/index.html", context)
 
 
 def get_user(request, user_id):
-    return HttpResponse(f"User get to come! {user_id}")
+    return HttpResponse(f"User details to come! {user_id}")
+
+
+def create_user(request):
+    return HttpResponse("TODO:!")
 
 
 def get_user_details(request, user_id):
